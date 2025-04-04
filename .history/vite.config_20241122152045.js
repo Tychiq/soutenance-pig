ca383863d2693c1093fb55b34c67a5a49201fe4f
Tychiq <tychique.allagbe77@gmail.com>
2025-04-04 16:35:59 +0100

    Second commit

--- .history/vite.config_20241122152045.js
+++ .history/vite.config_20241122152045.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    build: {
-        chunkSizeWarningLimit: 1000,  // Increase the limit to 1MB (default is 500kB)
-      },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
